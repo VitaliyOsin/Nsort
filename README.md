3edf7e9cd98d45018fc5108fd3fffdc58352b768
VitaliyOsin <d_dva@list.ru>
2021-03-07 00:45:07 +0700

    Function Nsor ot working!

---
+++ README.md
@@ -0,0 +1,1 @@
+Function for sorting numeric arrays
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Nsort');
+const arr = [1,34,2,7,12,4,5,6,4,-6]
+function nsort(ar){
+	const resar = [];
+	const rowEnd =  Math.max.apply(null,ar);
+	
+	const rowStart = Math.min.apply(null, ar);
+	console.log(rowStart, rowEnd);
+	for(let i = rowStart; i < rowEnd; i++){
+		const v = ar.indexOf(i);
+		if(ar.indexOf(i)>=0){
+			resar.push(ar[v]);
+		} 
+	}
+	return resar;
+}
+
+console.log(nsort(arr));
